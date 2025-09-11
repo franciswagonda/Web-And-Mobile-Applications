9d6afeb2d682d7b0b63926a624aad07d8b5847fe
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-09-11 20:36:06 +0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,19 @@
+//variables///they are containers for storing data values
+
+//var, let, const
+
+let a = 5;
+let b = 15;
+
+const myname = "Francis";
+
+let c = a + b;
+
+console.log(c);
+
+console.log(myname);
+
+
+let first_prompted_number = 10;
+
+let StudentAccessNumber = "B33245"  //camel case;
