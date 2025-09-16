5bb67872d024ae4f2f6f336bde9b8ba2789b30fa
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-09-16 21:15:14 +0300

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+//booleans
+
+let age = 18;
+let canvote = age >= 18;
+console.log(canvote); //true
+
+
+let isFemale = true;
+console.log(isFemale); //true
+
+
+
+//this is my age
+let useAge =21 ;//user's age
+
+
+/*
+yoooo 
+you good
+ye sirrr
+*/
+
+
+const myname = prompt("Enter your name");
+
+var myAge = prompt("Enter your age");
+
+console.log("My name is " + myname + " and I am " + myAge + " years old.");
