f7b9025e4d47598544d8bd899b7410288c807088
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-09-25 10:29:12 +0300

    new file:   conditional.js

---
+++ conditional.js
@@ -0,0 +1,14 @@
+
+let coursework = 45;
+
+let exammark = 30;
+
+let finalmark = (coursework + exammark);
+
+
+if (finalmark < 50) {
+    console.log("Fail");
+} 
+else {
+    console.log("Pass");
+}
